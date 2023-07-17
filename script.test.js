9116e8ef4e60248491a5e701c364cc7d5bb09935
gnunez-dev <gnunez.dev@gmail.com>
2023-07-17 00:01:38 -0500

    added describe - jest

--- script.test.js
+++ script.test.js
@@ -11,16 +11,20 @@ it('is a hello test', () => {
   expect('hello').toBe('hello')
 })
 
-it('is searching google', () => {
-  expect(googleSearch('testtest', dbMock)).toEqual([])
-  expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
-})
+describe('googleSearch', () => {
 
-it('work with undefined and null input', () => {
-  expect(googleSearch(undefined, dbMock)).toEqual([])
-  expect(googleSearch(null, dbMock)).toEqual([])
-})
+  it('is searching google', () => {
+    expect(googleSearch('testtest', dbMock)).toEqual([])
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+  })
 
-it('does not return more than 3 matches', () => {
-  expect(googleSearch('.com', dbMock).length).toEqual(3)
+  it('work with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([])
+    expect(googleSearch(null, dbMock)).toEqual([])
+  })
+
+  it('does not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3)
+  })
+
 })
