ac0856fec3ebbbcbdb630e2252e3db38d13283b6
gnunez-dev <gnunez.dev@gmail.com>
2023-07-18 19:48:21 -0500

    added return option

--- script2.test.js
+++ script2.test.js
@@ -1,6 +1,9 @@
 const fetch = require('node-fetch');
 const swapi = require('./script2');
 
+//Test with a function that return a promise.
+
+//option done
 it('calls swapi to get people with async/await', (done) => {
   expect.assertions(1)
   swapi.getPeople(fetch)
@@ -10,11 +13,11 @@ it('calls swapi to get people with async/await', (done) => {
     })
 })
 
-it('calls swapi to get people with a promise', (done) => {
+//opntio return
+it('calls swapi to get people with a promise', () => {
   expect.assertions(1)
-  swapi.getPeoplePromise(fetch)
+  return swapi.getPeoplePromise(fetch)
     .then( data => {
       expect(data.count).toEqual(87)
-      done()
     })
 })
